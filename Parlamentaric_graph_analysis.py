2f2489fc25e055db00ec2a5cedc7313490ce58da
amiriz <amir.izhaky@gmail.com>
2019-12-11 16:56:11 +0200

    finished the print properties functions for question 2

--- Parlamentaric_graph_analysis.py
+++ Parlamentaric_graph_analysis.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib import collections
+import copy
 
 g = None
 
@@ -10,50 +11,60 @@ def preprocess_graph():
     small_weight_edges = [(u, v) for (u, v, d) in g.edges(data=True) if d['weight'] < 3]
     g.remove_edges_from(small_weight_edges)
 
-""""
-def show_deg_dest():
+
+def get_degree_dest():
+    global g
+    degree_dest = {}
+
+    for node in g.nodes:
+        degree = g.degree(node)
+        if degree not in degree_dest:
+            degree_dest[degree] = 1
+        else:
+            degree_dest[degree] += 1
+    return degree_dest
+
+
+def get_diameter():
+    global g
+    diameter = -1
+
+    for node in g.nodes:
+        cur_node_shortest_path = nx.shortest_path_length(g, node)
+        for length in cur_node_shortest_path.values():
+            if diameter < length:
+                diameter = length
+    return diameter
+
+
+def get_aspl():
+    global g
+    shortest_paths = list()
+
+    for node in g.nodes:
+        cur_node_shortest_path = list(nx.shortest_path_length(g, node).values())
+        shortest_paths += cur_node_shortest_path
+
+    return sum(shortest_paths) / len(shortest_paths)
+
+
+def print_properties():
     global g
 
-    degree_sequence = sorted([d for n, d in g.degree()], reverse=True)  # degree sequence
-    # print "Degree sequence", degree_sequence
-    degree_count = len(degree_sequence)
-    deg, cnt = zip(*degree_count.items())
-
-    fig, ax = plt.subplots()
-    plt.bar(deg, cnt, width=0.80, color='b')
-
-    plt.title("Degree Histogram")
-    plt.ylabel("Count")
-    plt.xlabel("Degree")
-    ax.set_xticks([d + 0.4 for d in deg])
-    ax.set_xticklabels(deg)
-
-    # draw graph in inset
-    plt.axes([0.4, 0.4, 0.5, 0.5])
-    Gcc = g.subgraph(sorted(nx.connected_components(g), key=len, reverse=True)[0])
-    pos = nx.spring_layout(g)
-    plt.axis('off')
-    nx.draw_networkx_nodes(g, pos, node_size=20)
-    nx.draw_networkx_edges(g, pos, alpha=0.4)
-
-    plt.show()
-"""
-
-
-def check_properties():
-    global g
-    den = nx.density(g)             # int
-    dia = nx.diameter(g)            # int
-    clus = nx.clustering(g)
+    print("density = " + str(nx.density(g)))
+    print("diameter = " + str(get_diameter()))
+    print("Clustering Coefficient = " + str(nx.clustering(g)))
+    print("average shortest path length = " + str(get_aspl()))
+    print("Degree Distribution = " + str(get_degree_dest()))
 
 
 def main():
     global g
     g = nx.read_gexf("C:\\Users\\amiri\\Desktop\\net_il2015-2018.gexf")
     preprocess_graph()
-    # show_deg_dest()
-    # nx.draw(g)
-    # plt.show()
+    print_properties()
+    nx.draw(g)
+    plt.show()
 
 
 if __name__ == '__main__':
